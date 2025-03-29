333744bc5e95b4cc159f7159c1858ee13fffcd27
Diego Masson <diego1masson@gmail.com>
2025-03-28 21:24:22 -0300

    meu projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -16,18 +16,18 @@ public class ContaTerminal {
 
         // Solicita e recebe os dados via terminal
         System.out.print("Por favor, digite o número da Conta: ");
-        numeroConta = scanner.nextInt(1021); // Lê o número da conta
+        numeroConta = scanner.nextInt(); // Lê o número da conta
 
         scanner.nextLine(); // Consumir a quebra de linha deixada pelo nextInt
 
         System.out.print("Por favor, digite o número da Agência: ");
-        agencia = scanner.nextLine(0678); // Lê o número da agência
+        agencia = scanner.nextLine(); // Lê o número da agência
 
         System.out.print("Por favor, digite o nome do Cliente: ");
-        nomeCliente = scanner.nextLine(MARIO ANDRADE); // Lê o nome do cliente
+        nomeCliente = scanner.nextLine(); // Lê o nome do cliente
 
         System.out.print("Por favor, digite o saldo da conta: ");
-        saldo = scanner.nextDouble(237.48); // Lê o saldo da conta
+        saldo = scanner.nextDouble(); // Lê o saldo da conta
         
         // Exibe a mensagem formatada
         System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, "
